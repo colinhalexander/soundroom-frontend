8ed970fca34abafb320bf065853345b1eeff9bdf
colinhalexander <chalexander@icloud.com>
2019-11-23 16:29:22 -0700

    set up scss, continue styling landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import '../stylesheets/LandingPage.css'
 import speakerLogo from '../media/stereo-512-white.png'
+import spotifyLogo from '../media/Spotify_Logo_RGB_White.png'
 
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 export default function LandingPage() {
+
+  const signInWithSpotify = () => window.location.href = "http://localhost:3000/auth"
+
   return (
     <div className="landing-page">
       <hgroup className="logo">
         <h1>SoundR<img src={speakerLogo} alt=""/><img src={speakerLogo} alt=""/>m</h1>
       </hgroup>
-      <p>SoundRoom is a collaborative playlist-maker, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
-      <a id="sign-in-btn" href="http://localhost:3000/auth">Sign in with Spotify</a>
-      <Footer />
+      <p>Make playlists. Invite your friends.</p>
+      <div id="sign-in-btn" onClick={signInWithSpotify}>
+        <p>Sign in with</p><img src={spotifyLogo} alt="spotify logo" />
+      </div>
+      {/* <Footer /> */}
     </div>
   )
 }
