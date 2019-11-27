9e62072e6bbda06eeb9a16309338e5ff4faf89c3
colinhalexander <chalexander@icloud.com>
2019-11-26 18:45:06 -0700

    move Spotify player logic into SpotifyPlayer component

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,18 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function SpotifyPlayer({ album, artists, name, uri }) {
+export default class SpotifyPlayer extends Component {
 
-  const artistsString = () => {
-    return makeListFromArray( artists.map(artist => artist.name) )
+  state = {
+    player: null,
+    playerState: null
   }
 
-  return (
-    <div className="spotify-player">
-      <img src={album.images[0].url} alt="album art" />
-      <h4>{name}</h4>
-      <p>{artistsString}</p>
-    </div>
-  )
+  componentDidMount() {
+    const { playerReady, user } = this.props,
+          isPlayerReady = setInterval(() => {
+            if (playerReady && user) {
+              this.initializePlayer()
+              clearInterval(isPlayerReady)
+            }
+          }, 1000);
+
+    // get and display currently playing track
+  }
+
+  initializePlayer = () => {
+    const { Spotify } = window,
+          player = new Spotify.Player({
+            name: "Soundroom",
+            getOAuthToken: async (callback) => {
+              const accessToken = await fetch(`http://localhost:3000/users/${this.props.user.id}/token`)
+                .then(response => response.json())
+
+              callback(encryptor.decrypt(accessToken))
+            }
+          })
+  
+    this.setState({ player })
+    this.connectPlayer(player)
+  }
+
+  connectPlayer = (player) => {
+    player.connect()
+      .then(success => {
+        if (!success) {
+          alert("Unable to connect to Spotify Player")
+        } else {
+          this.getPlayerState(player)
+        }
+      })
+  }
+
+  getPlayerState = (player) => {
+    player.getCurrentState()
+      .then(state => {
+        console.log("spotify player state:", state)
+        if (state) {
+          this.setState({ playerState: state })
+        }
+      })
+  }
+
+  // artistsString = () => {
+  //   return makeListFromArray( artists.map(artist => artist.name) )
+  // }
+
+  render() {
+    const {}
+
+    return (
+      <div className="spotify-player">
+        <p>Spotify Player Here</p>
+        {/* <img src={album.images[0].url} alt="album art" /> */}
+        {/* <h4>{name}</h4> */}
+        {/* <p>{artistsString}</p> */}
+      </div>
+    )
+  }
 }
 
 function makeListFromArray(strings) {
--- src/containers/SoundRoom.js
+++ src/containers/SoundRoom.js
@@ -14,52 +14,22 @@ export default class SoundRoom extends Component {
   }
 
   componentDidMount() {
-    const { playerReady, user } = this.props,
-          isPlayerReady = setInterval(() => {
-            if (playerReady && user) {
-              this.initializePlayer()
-              clearInterval(isPlayerReady)
-            }
-          }, 1000);
-
     const { soundroom } = this.props.location.state
     if (soundroom) {
       this.setState({ soundroom })
     }
 
-    // get and display currently playing track
-
     // create new playlist for user, named after soundroom
-  }
-
-  initializePlayer = () => {
-    const { Spotify } = window,
-          player = new Spotify.Player({
-            name: "Soundroom",
-            getOAuthToken: async (callback) => {
-              const accessToken = await fetch(`http://localhost:3000/users/${this.props.user.id}/token`)
-                .then(response => response.json())
-
-              callback(encryptor.decrypt(accessToken))
-            }
-          })
-  
-    this.setState({ playerID: player._options.id })
-
-    player.connect().then((success) => {
-      if (!success) {
-        alert("Unable to connect to Spotify Player")
-      }
-    })
   }
 
   render() {
     const { soundroom } = this.state
+    const { user, playerReady } = this.props
 
     return (
       <section className="soundroom">
         <p>{soundroom.name}</p>
-        <SpotifyPlayer  />
+        <SpotifyPlayer user={user} playerReady={playerReady} />
       </section>
     )
   }
